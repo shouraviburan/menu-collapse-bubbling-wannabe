e0b082b71c02a9c15fd6374e4d65534edb4b8b2d
shouraviburan <dunia@riseup.net>
2021-01-25 18:35:16 +0100

    version 2.0

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+const container = document.getElementById("container");
+
+container.addEventListener("click", function(event){
+
+    const childrenArray = event.target.children
+    
+    for (i=0; i < childrenArray.length; i++ ) {
+        if (childrenArray[i].style.display != 'block') {
+            childrenArray[i].style.display = 'block';
+        } else  {
+            childrenArray[i].style.display = 'none';
+        }s
+    }
+    
+})
+
+
